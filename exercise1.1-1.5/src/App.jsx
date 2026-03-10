7f58bcdb14989a6e37426bdc44f4182b09a74119
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-03-10 14:20:54 +0100

    course exercise 1.2

--- exercise1.1-1.5/src/App.jsx
+++ exercise1.1-1.5/src/App.jsx
@@ -6,9 +6,9 @@ const Header = (props) => {
 
 const Content = (props) => {
   return <div>
-    <p>{props.part1}: {props.exercises1}</p>
-    <p>{props.part2}: {props.exercises2}</p>
-    <p>{props.part3}: {props.exercises3}</p>
+    <Part part={props.part1} exercise={props.exercises1}></Part>
+    <Part part={props.part2} exercise={props.exercises2}></Part>
+    <Part part={props.part3} exercise={props.exercises3}></Part>
   </div>
 }
 
@@ -17,6 +17,12 @@ const Total = (props) => {
     <p>
       Total number of exercises is {props.exercises1 + props.exercises2 + props.exercises3}
     </p>
+  </div>
+}
+
+const Part = (props) => {
+  return <div>
+    <p>{props.part} {props.exercise}</p>
   </div>
 }
 
