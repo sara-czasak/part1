d36562ebe595122ecc76347de60ac7cda3d8b9ac
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-03-13 15:18:28 +0100

    course exercise 2.5

--- exercise1.1-1.5/src/App.jsx
+++ exercise1.1-1.5/src/App.jsx
@@ -1,11 +1,10 @@
+import Course from './components/Course'
 
 const Header = ({name}) => {
-    // console.log("HEADER course", name)
     return <h1>{name}</h1>
 }
 
 const Content = ({parts}) => {
-    // console.log('Content ', parts[0].name)
     return <div>
         <ul>
             {parts.map(part =>
@@ -16,46 +15,18 @@ const Content = ({parts}) => {
 }
 
 const Part = ({content}) => {
-    // console.log("Part ", content.name)
     return <li>
         {content.name} {content.exercises}
            </li>
 }
 
 const ExerciseSum = ({parts}) => {
-    // let sum = 0
     const total = parts.reduce((accumulator, currentValue) => {
         return accumulator += currentValue.exercises
     }, 0)
     console.log(total)
     return <div>
         <h4>The total of exercises is: {total}</h4>
-    </div>
-
-
-    // <div>
-    //     {parts.map(part => {
-    //         total += part.exercises
-    //     })}
-    //     <h4>The total of exercises is: {total}</h4>
-    // </div>
-}
-
-const Course = ({courses}) => {
-    console.log(courses)
-    
-    return <div>
-        {courses.map(course => {
-            console.log('name, ', course.name)
-            console.log('parts, ', course.parts)
-            return <div>
-                <Header name={course.name} />
-                <Content parts={course.parts} />
-                <ExerciseSum parts={course.parts} />
-            </div>
-            
-        })}
-     
     </div>
 }
 
@@ -104,30 +75,8 @@ const App = () => {
         ]
       }
     ]
-
-    const course = {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10,
-            id: 1
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7,
-            id: 2
-          },
-          {
-            name: 'State of a component',
-            exercises: 14,
-            id: 3
-          }
-        ]
-      }
   
     return <Course courses={courses} />
   }
   
-  export default App
+  export {App, Header, Content, ExerciseSum}
--- exercise1.1-1.5/src/components/Course.jsx
+++ exercise1.1-1.5/src/components/Course.jsx
@@ -1,11 +1,21 @@
-const CourseHeader = ({courses}) => {
+import {Header, Content, ExerciseSum} from '../App'
+
+const Course = ({courses}) => {
+    console.log(courses)
+    
     return <div>
         {courses.map(course => {
-            <h1>
-                {course.content}
-            </h1>
+            console.log('name, ', course.name)
+            console.log('parts, ', course.parts)
+            return <div key={course.id}>
+                <Header name={course.name} />
+                <Content parts={course.parts} />
+                <ExerciseSum parts={course.parts} />
+            </div>
+            
         })}
+     
     </div>
 }
 
-export default CourseHeader
+export default Course
